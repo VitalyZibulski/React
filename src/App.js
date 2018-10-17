a887237e73d017403f1c4fdb0efcef8f0474b5b0
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-17 12:36:53 +0300

    add styles

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
 
         return (
             <div style={divStyle}>
-              <h1>Hello world!</h1>
+              <h1 style={{color:'blue',fontSize:'20px'}}>Hello world!</h1>
+                {/*'font-size':'20px' or fontSize:'20px'*/}
             </div>
         );
     }
