dec2f3f4090a99aa8eea051911c6b33984a83338
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-10-17 23:38:07 +0300

    use dynamic class

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import './Car.css'
 
-export default props => (
-    <div className="Car">
-        <p>This is car component</p>
-        <p>Car name:<strong>{props.name}</strong></p>
-        <p>Year:<strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-)
+export default props => {
+    const inputClasses = ['input']
+
+    if(props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if(props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
+    return (
+        <div className="Car">
+            <p>This is car component</p>
+            <p>Car name:<strong>{props.name}</strong></p>
+            <p>Year:<strong>{props.year}</strong></p>
+            <input
+                type="text"
+                onChange={props.onChangeName}
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
+}
