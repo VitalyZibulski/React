dee061d95d9bd52ee120440190d62f4dcb628b82
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-11-05 18:58:45 +0300

    use ref

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types'
 import withClass from '../hoc/withClass'
 
 class Car extends React.Component {
+    componentDidMount() {
+        if(this.props.index === 0) {
+            this.inputRef.focus()
+        }
+    }
 
     render(){
 
@@ -25,6 +30,7 @@ class Car extends React.Component {
                 <p>Car name:<strong>{this.props.name}</strong></p>
                 <p>Year:<strong>{this.props.year}</strong></p>
                 <input
+                    ref={(inputRef) => this.inputRef = inputRef }
                     type="text"
                     onChange={this.props.onChangeName}
                     value={this.props.name}
@@ -39,6 +45,7 @@ class Car extends React.Component {
 Car.propTypes = {
     name: PropTypes.string.isRequired,
     year: PropTypes.number,
+    index: PropTypes.number,
     onChangeName: PropTypes.func,
     onDelete: PropTypes.func
 }
